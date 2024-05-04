c8b1059c745e6ba0fecd67d057f8a4bd4367ddc6
rsuty <mistergamer37@gmail.com>
2024-05-03 19:58:05 -0700

    add asteroids and children asteroids

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,9 +2,36 @@ extends Node2D
 
 @onready var bullets =  $Bullets
 @onready var player = $Player
+@onready var asteroids = $Asteroids
+
+var asteroid_scene = preload("res://scenes/asteroid.tscn")
 
 func _ready():
 	player.connect("bullet_shot", _on_player_bullet_shot)
+	
+	for asteroid in asteroids.get_children():
+		asteroid.connect("exploded", _on_asteroid_exploded)
 
 func _on_player_bullet_shot(b):
 	bullets.add_child(b)
+
+func _process(delta):
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+
+func _on_asteroid_exploded(pos, size):
+	for i in range(2):
+		match size:
+			Asteroid.AsteroidSize.LARGE:
+				spawn_asteroid(pos, Asteroid.AsteroidSize.MEDIUM)
+			Asteroid.AsteroidSize.MEDIUM:
+				spawn_asteroid(pos, Asteroid.AsteroidSize.SMALL)
+			Asteroid.AsteroidSize.SMALL:
+				pass
+
+func spawn_asteroid(pos, size):
+	var a = asteroid_scene.instantiate()
+	a.global_position = pos
+	a.size = size
+	a.connect("exploded", _on_asteroid_exploded)
+	asteroids.add_child(a)
